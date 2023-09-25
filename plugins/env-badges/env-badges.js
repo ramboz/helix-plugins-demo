b9ac486e637c84b38c43c0c8c30f7fe53e232608
Julien Ramboz <ramboz@adobe.com>
2023-09-25 17:51:29 +0200

    feat: build demo implementation

--- plugins/env-badges/env-badges.js
+++ plugins/env-badges/env-badges.js
@@ -1,5 +1,8 @@
 
 export const loadEager = () => console.error('Never called');
 
-export const loadLazy = () => document.body.classList.add(
-  window.location.hostname === 'localhost' ? 'is-dev' : 'is-stage');
+export const loadLazy = () => {
+  if (window.location.hostname === 'localhost') document.body.classList.add('is-dev');
+  if (window.location.hostname.endsWith('.hlx.page')) document.body.classList.add('is-stage');
+  if (window.location.hostname.endsWith('.hlx.live')) document.body.classList.add('is-live');
+}
